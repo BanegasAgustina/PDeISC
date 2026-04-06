04400f5d4b9f505ae4e6b8f5096f0ccae38a2add
Banegas , Agustina <133693422+BanegasAgustina@users.noreply.github.com>
2026-04-06 19:57:23 -0300

    Update ejercicio1.js

--- ejercicio5/proyecto/ejercicio1.js
+++ ejercicio5/proyecto/ejercicio1.js
@@ -2,7 +2,7 @@ export function getResultado() {
   return {
     titulo: 'Ejercicio 1 — Hola Mundo',
     operaciones: [
-      { operacion: 'Mensaje', resultado: 'Hola mundo desde Node.js' }
+      { operacion: 'Mensaje', resultado: 'Hola mundo desde Node.js\nfin' }
     ]
   };
 }
