0af1d38b60c7fb486d071f5df80217ce18c0b937
Banegas , Agustina <133693422+BanegasAgustina@users.noreply.github.com>
2026-04-13 19:56:37 -0300

    Update cadapag.js

--- NodeJs_O2/cadapag.js
+++ NodeJs_O2/cadapag.js
@@ -1,14 +1,1 @@
-// js/calculadora.js
-export function calculadora() {
-  return `<h1> Calculadora</h1><p>Contenido de la calculadora</p>`;
-}
 
-// js/reloj.js
-export function reloj() {
-  return `<h1> Reloj</h1><p>Contenido del reloj</p>`;
-}
-
-// js/clima.js
-export function clima() {
-  return `<h1> Clima</h1><p>Contenido del clima</p>`;
-}
